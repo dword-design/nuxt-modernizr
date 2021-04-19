f659d269f7cc57ffb16856478559d0c70f966ddd
dword <info@dword-design.de>
2021-04-19 15:55:05 +0000

    fix: fix deprecated dependency

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import pushPlugins from '@dword-design/nuxt-push-plugins'
 import modernizr from 'modernizr'
+import nuxtPushPlugins from 'nuxt-push-plugins'
 import P from 'path'
 
 export default async function (moduleOptions) {
   const options = { ...this.options.modernizr, ...moduleOptions }
 
   const code = await new Promise(resolve => modernizr.build(options, resolve))
-  pushPlugins(this, {
+  nuxtPushPlugins(this, {
     fileName: P.join('nuxt-modernizr', 'plugin.js'),
     mode: 'client',
     options: code,
