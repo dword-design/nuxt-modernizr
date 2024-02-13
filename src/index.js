200393b688e82ebaff0ea222315b6d11548338bd
Sebastian Landwehr <info@sebastianlandwehr.com>
2024-02-13 22:34:12 +0000

    fix: foo

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
+import { addPluginTemplate, isNuxt3 as isNuxt3Try } from '@nuxt/kit'
 import modernizr from 'modernizr'
 import { createRequire } from 'module'
 import nuxtPushPlugins from 'nuxt-push-plugins'
 import P from 'path'
 
-const _require = createRequire(import.meta.url)
+const resolver = createRequire(import.meta.url)
 
-export default async function (moduleOptions) {
-  const options = { ...this.options.modernizr, ...moduleOptions }
+export default async function (moduleOptions, nuxt) {
+  let isNuxt3 = true
+  try {
+    isNuxt3 = isNuxt3Try()
+  } catch {
+    isNuxt3 = false
+  }
+  nuxt = nuxt || this
+
+  const options = { ...nuxt.options.modernizr, ...moduleOptions }
 
   const code = await new Promise(resolve => modernizr.build(options, resolve))
-  nuxtPushPlugins(this, {
-    fileName: P.join('nuxt-modernizr', 'plugin.js'),
-    mode: 'client',
-    options: code,
-    src: _require.resolve('./plugin.js.template'),
-  })
+  if (isNuxt3) {
+    addPluginTemplate({
+      filename: P.join('nuxt-modernizr', 'plugin.js'),
+      getContents: () => `export default defineNuxtPlugin(() => { ${code} })`,
+      mode: 'client',
+    })
+  } else {
+    nuxtPushPlugins(nuxt, {
+      fileName: P.join('nuxt-modernizr', 'plugin.js'),
+      mode: 'client',
+      options: code,
+      src: resolver.resolve('./plugin.js.template'),
+    })
+  }
 }
